aed7e3c5a1007360e9a58591a562aab6b3eba754
Oce <oceEeco@ymail.com>
2016-09-02 10:36:56 +0200

    Fixed writting mistake
    Fixed error that was writing doublons

--- nameGenerator.scala
+++ nameGenerator.scala
@@ -8,14 +8,16 @@ object nameGenerator {
     val vowel_min = List('a', 'e', 'i', 'o', 'u', 'y')
     val conso_maj = maj.filterNot(vowel_maj.contains(_))
     val conso_min = min.filterNot(vowel_min.contains(_))
-    println(conso_maj)  
-    println(conso_min)
+
     val Vcv = vowel_maj.map { V => conso_min.map { c => vowel_min.map { v => V.toString()+c.toString()+v.toString() } } }.flatten.flatten
     val Cvc = conso_maj.map { C => vowel_min.map { v => conso_min.map { c => C.toString()+v.toString()+c.toString() } } }.flatten.flatten
+    
     val pw = new PrintWriter(new File("generatedNames.txt" ))
-    pw.write("Vcv Cvc\n_______\n")
-    Vcv.map { Vcv => Cvc.map { Cvc => pw.write(Vcv+" "+Cvc+"\n") } }
+    pw.write("Vcv\n_______\n")
+    Vcv.map { Vcv => pw.write(Vcv+"\n") }
+    pw.write("\nCvc\n_______\n")
+    Vcv.map { Cvc => pw.write(Cvc+"\n") }
     pw.close
-  }
-  
+    println(Vcv.size+" "+Cvc.size)
+  } 
 }
